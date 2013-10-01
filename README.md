1719eebdb3b898ae6e4f4fbd44bb1a979b4e5dc7
Eric Dalquist <eric.dalquist@gmail.com>
2013-10-01 00:43:28 -0700

    Converted to using Wand for tokenization

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ token_extract
 =============
 
 Utility scripts for extracting D&amp;D tokens from PDFs
+
+
+Installation
+=============
+sudo easy_install Wand
--- oots_minis.py
+++ oots_minis.py
@@ -2,12 +2,14 @@
 
 from optparse import OptionParser
 import subprocess
-import extract
+import token_util
 import glob
 import os
 import re
 import math
 import locale
+from wand.image import Image
+from wand.display import display
 
 encoding = locale.getdefaultlocale()[1]
 
@@ -37,7 +39,7 @@ if (options.endPage is None):
 print ('Parsing PDF', options.filename, 'from', options.startPage, 'to', options.endPage)
 for page in range(options.startPage, options.endPage):
 	print('Page: ', page)
-	extract.extractToPng(options.filename, page, page, "pdfimg")
+	token_util.extractToPng(options.filename, page, page, "pdfimg")
 
 
 	# Get list of PNG files
@@ -61,59 +63,74 @@ for page in range(options.startPage, options.endPage):
 			os.mkdir(origDir)
 
 		tokenFileName = pageNum + "/token-" + pageNum + "-" + m.group(2) + ".png"
-		backTokenFileName = pageNum + "/token-" + pageNum + "-" + m.group(2) + "-0.png"
-		frontTokenFileName = pageNum + "/token-" + pageNum + "-" + m.group(2) + "-1.png"
+		backTokenFileName = pageNum + "/token-" + pageNum + "-" + m.group(2) + "-b.png"
+		frontTokenFileName = pageNum + "/token-" + pageNum + "-" + m.group(2) + "-f.png"
 
 		print ("Merging: ", fl1, " ", fl2)
 
-		# Apply the alpha mask to the base image
-		subprocess.call(["composite", "-compose", "copy_opacity", "+matte", fl2, fl1, tempFile])
+		with Image(filename=fl1) as fullToken:
+			with Image(filename=fl2) as mask:
+				# Apply the alpha mask to the base image
+				fullToken.composite_channel('all_channels', mask, 'copy_opacity', 0, 0)
 
+				# Calcuate the crop data
+				cropWidth = fullToken.size[0]
+				cropHeight = math.floor(fullToken.size[1]/2)
+				fullHeight = fullToken.size[1]
+
+				# Split the token vertically into front/back tokens
+				with fullToken[0:cropWidth, 0:cropHeight] as tokenBack:
+					# Rotate and flip the back token
+					tokenBack.rotate(180)
+					tokenBack.flop()
+
+					with fullToken[0:cropWidth, cropHeight:fullHeight] as tokenFront:
+						tokens = [tokenBack, tokenFront]
+
+						# Trim excess whitespace
+						[ x.trim() for x in tokens ]
+
+						tokenBack.width = 1000
+
+						tokenBack.save(filename=backTokenFileName)
+						tokenFront.save(filename=frontTokenFileName)
+
+				
 		# Move original files into orig dir
 		os.rename(fl1, origDir + "/" + fl1)
 		os.rename(fl2, origDir + "/" + fl2)
 
-		# Split token into well-named file
-		subprocess.call(["convert", tempFile, "-crop", "100%x50%", tokenFileName])
+		# cmd = subprocess.Popen("identify -ping -format '%W/%H' " + frontTokenFileName, shell=True, stdout=subprocess.PIPE)
+		# for line in cmd.stdout.readlines():
+		# 	line = line.decode(encoding)
+		# 	if "/" in line:
+		# 		dimensions = line.split("/")
+		# 		size = max(int(dimensions[0]), int(dimensions[1]))
+		# 		halfSize = math.floor(size / 2)
 
-		# Rotate and flip the "back" view
-		subprocess.call(["convert", backTokenFileName, "-rotate", "180", "-flop", backTokenFileName])
+		# 		size = str(size)
+		# 		halfSize = str(halfSize)
 
-		# Remove any extra border from the images
-		subprocess.call(["convert", frontTokenFileName, "-trim", "+repage", frontTokenFileName])
-		subprocess.call(["convert", backTokenFileName, "-trim", "+repage", backTokenFileName])
-
-		cmd = subprocess.Popen("identify -ping -format '%W/%H' " + frontTokenFileName, shell=True, stdout=subprocess.PIPE)
-		for line in cmd.stdout.readlines():
-			line = line.decode(encoding)
-			if "/" in line:
-				dimensions = line.split("/")
-				size = max(int(dimensions[0]), int(dimensions[1]))
-				halfSize = math.floor(size / 2)
-
-				size = str(size)
-				halfSize = str(halfSize)
-
-				# Make Square Tokens
-				subprocess.call(["convert", backTokenFileName, "-background", "none", "-gravity", "center", "-extent", size + "x" + size, backTokenFileName])
-				subprocess.call(["convert", frontTokenFileName, "-background", "none", "-gravity", "center", "-extent", size + "x" + size, frontTokenFileName])
+		# 		# Make Square Tokens
+		# 		subprocess.call(["convert", backTokenFileName, "-background", "none", "-gravity", "center", "-extent", size + "x" + size, backTokenFileName])
+		# 		subprocess.call(["convert", frontTokenFileName, "-background", "none", "-gravity", "center", "-extent", size + "x" + size, frontTokenFileName])
 				
 				# Add background
 				# TODO os.chdir()
-				subprocess.call(["convert", backTokenFileName, "-alpha", "on",
-					"(", "+clone", "-alpha", "Transparent", "-fill", "#FF000050", "-draw", "'circle " + halfSize + "," + halfSize + " " + halfSize + "," + size + "'", ")",
-					"-compose", "dst-over", "-composite", backTokenFileName])
-				subprocess.call(["convert", frontTokenFileName, "-alpha", "on",
-					"(", "+clone", "-alpha", "Transparent", "-fill", "#FF000050", "-draw", "'circle " + halfSize + "," + halfSize + " " + halfSize + "," + size + "'", ")",
-					"-compose", "dst-over", "-composite", frontTokenFileName])
+				# subprocess.call(["convert", backTokenFileName, "-alpha", "on",
+				# 	"(", "+clone", "-alpha", "Transparent", "-fill", "#FF000050", "-draw", "'circle " + halfSize + "," + halfSize + " " + halfSize + "," + size + "'", ")",
+				# 	"-compose", "dst-over", "-composite", backTokenFileName])
+				# subprocess.call(["convert", frontTokenFileName, "-alpha", "on",
+				# 	"(", "+clone", "-alpha", "Transparent", "-fill", "#FF000050", "-draw", "'circle " + halfSize + "," + halfSize + " " + halfSize + "," + size + "'", ")",
+				# 	"-compose", "dst-over", "-composite", frontTokenFileName])
 
 				
 				# convert  006/token-006-000-0.png  -alpha on  \
 			 #         \( +clone -alpha Transparent -fill \#FF000050 -draw 'circle 401,401 401,803' \) \
 			 #         -compose dst-over -composite    token.png
 
-				break
+				# break
 
 
 
-	os.remove(tempFile)
+	# os.remove(tempFile)
